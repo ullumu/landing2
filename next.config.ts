be835950b219444dbc21add62e0f5afc99009706
ullumu <ullumu18@gmail.com>
2026-06-25 13:32:44 +0700

    test deploy github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",       // aktifkan static export
+  basePath: "/landing2",  // ganti sesuai nama repo
 };
 
 export default nextConfig;
